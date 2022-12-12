64a7edcd6bc940686df60c5353dcd57fab7dab00
neel.citrusbug <neel.citrusbug@gmail.com>
2022-12-12 18:30:38 +0530

    update value on input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
-  
+  displayVal = '';
   getName(name:string){
     alert('name: ' + name)
   }
 
   getData(val:string){
     console.log(val)
+  }
+
+  updateValue(val:string){
+    this.displayVal = val;
   }
 }
