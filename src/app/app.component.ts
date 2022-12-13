f2eb6a08e5f72a3e01517905e3226470eeb4aaf0
neel.citrusbug <neel.citrusbug@gmail.com>
2022-12-13 10:34:39 +0530

    for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
+  users=[
+    {name:'Neel',phone:'1111',email:'neel@test.com',accounts:['facebook','gmail','linkedin']},
+    {name:'Krupal',phone:'2222',email:'krupal@test.com',accounts:['Youtube','yahoo','linkedin']},
+    {name:'Sandeep',phone:'3333',email:'sandeep@test.com',accounts:['Yahoo','insta','linkedin']},
+    {name:'Kishan',phone:'2299',email:'kishan@test.com',accounts:['Gmail','gmail','youtube']},
+
+  ]
 }
