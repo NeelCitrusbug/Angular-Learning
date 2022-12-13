21867950e6f62c8bff13d62c5f32c067563997b4
neel.citrusbug <neel.citrusbug@gmail.com>
2022-12-13 19:14:19 +0530

    reactive form and validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
+  reactiveForm = new FormGroup({
+    user:new FormControl('',[Validators.required, Validators.pattern('[a-zA-z]+$')]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
+  })
+
+  onSubmit(){
+    console.warn(this.reactiveForm.value)
+  }
+
+  get user(){
+    return this.reactiveForm.get('user')
+  }
+
+  get password(){
+    return this.reactiveForm.get('password')
+  }
 }
