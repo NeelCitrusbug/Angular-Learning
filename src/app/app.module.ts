bdc255ea48d4de9a5a15758c21205842abe5a1f3
neel.citrusbug <neel.citrusbug@gmail.com>
2022-12-14 18:20:51 +0530

    module routing with grouping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AdminModule} from './admin/admin.module'
+import {UserModule} from './user/user.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
